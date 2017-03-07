ae0d5f67fa94cbe19f22967766edcbb74d75f013
Olaf Kwant <olaf+github@zendesk.com>
2017-03-07 14:53:25 +1100

    and more comments

--- app.js
+++ app.js
@@ -575,7 +575,8 @@
     commitTicketTime: function(ticketTime) {
       ticketTime = ticketTime !== undefined ? ticketTime : this.elapsedTimeV2();
 
-      // For stay on ticket, we don't want the timer to start counting again.
+      // For stay on ticket, we don't want the timer to start counting again
+      // and calling resetNewTimers resets the pause state.
       var wasPaused = this.isPaused();
 
       // only update if ticketTime is > 0
